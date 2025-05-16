4382c820bc34692bdb242931dd708c755c8edd94
J. Reuben Wetherbee <wetherbe@sas.upenn.edu>
2025-05-16 11:26:25 -0400

    adds Unit export to tenure/Unitapi for backwards compatibility

--- src/tenure/unit-api.ts
+++ src/tenure/unit-api.ts
@@ -1,7 +1,8 @@
-import UnitApi from '../core/unit-api';
+import {UnitApi, Unit} from '../core/unit-api';
 
 /**
  * unit api moved to core folder
  */
 export { UnitApi } from '../core/unit-api';
+export type { Unit } from '../core/unit-api';
 export default UnitApi;
